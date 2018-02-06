a5e34d31af9bc895658eaeab9e94b30ea4da9416
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-02-06 16:39:45 -0600

    Added folders

---
+++ app/src/main/java/com/capstonegyg/gyg/UI/Profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.capstonegyg.gyg.UI.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.capstonegyg.gyg.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_screen);
+    }
+}
