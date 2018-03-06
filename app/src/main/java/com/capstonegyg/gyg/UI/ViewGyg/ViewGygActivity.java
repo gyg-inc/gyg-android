797b1121b62b944100661d28c2e5e02a0e9cedf4
IsuruNana <ipnanayakkara@CASS-231-04.ia.ua.edu>
2018-03-05 18:43:22 -0600

    Added Firebase data file

--- app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygActivity.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygActivity.java
@@ -7,4 +7,5 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class ViewGygActivity extends AppCompatActivity {
+
 }
---
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygData.java
@@ -0,0 +1,8 @@
+package com.capstonegyg.gyg.UI.ViewGyg;
+
+/**
+ * Created by ipnanayakkara on 3/5/2018.
+ */
+
+public class ViewGygData {
+}
