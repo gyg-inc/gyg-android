16d29fd8a8f59f167fd08cf9d1cf846c594ae430
ipnanayakkara <ipnanayakkara@CASS-231-04.ia.ua.edu>
2018-03-05 18:39:12 -0600

    Added view and post gyg files

---
+++ app/src/main/java/com/capstonegyg/gyg/UI/PostGyg/PostGygActivity.java
@@ -0,0 +1,11 @@
+package com.capstonegyg.gyg.UI.PostGyg;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ipnanayakkara on 3/5/2018.
+ */
+
+public class PostGygActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygActivity.java
@@ -0,0 +1,10 @@
+package com.capstonegyg.gyg.UI.ViewGyg;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ipnanayakkara on 3/5/2018.
+ */
+
+public class ViewGygActivity extends AppCompatActivity {
+}
