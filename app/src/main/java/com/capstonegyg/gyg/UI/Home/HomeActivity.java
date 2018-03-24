3d7718a921305212acf40127cb86ab7a551dd374
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-24 14:47:16 -0500

    Added button to go to view gyg

--- app/src/main/java/com/capstonegyg/gyg/UI/Home/HomeActivity.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/Home/HomeActivity.java
@@ -12,6 +12,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+
     }
 
 }
--- app/src/main/java/com/capstonegyg/gyg/logic/StartScreen.java
+++ app/src/main/java/com/capstonegyg/gyg/logic/StartScreen.java
@@ -1,14 +1,31 @@
 package com.capstonegyg.gyg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.capstonegyg.gyg.UI.ViewGyg.ViewGygActivity;
 
 public class StartScreen extends AppCompatActivity {
+
+    private Button viewGygs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
         // Jonathan Luetze
+
+        viewGygs = findViewById(R.id.view_gyg_button);
+
+        viewGygs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(this, ViewGygActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
