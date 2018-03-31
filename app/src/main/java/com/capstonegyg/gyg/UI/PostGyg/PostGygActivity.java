ef2b733ea9f8b70deb28b0c1c11d417b7a628bc7
Jonathan Luetze <jmluetze@crimson.ua.edu>
2018-03-31 16:29:05 -0500

    - Added Spinner functionality in PostGyg class to set up payment - Changed Post Gyg links and buttons from ListGyg - Removed Warnings

--- app/src/main/java/com/capstonegyg/gyg/UI/PostGyg/PostGygActivity.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/PostGyg/PostGygActivity.java
@@ -2,8 +2,12 @@ package com.capstonegyg.gyg.UI.PostGyg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.capstonegyg.gyg.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jonathan on 3/5/2018.
@@ -15,6 +19,24 @@ public class PostGygActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_gyg_screen);
+
+
+        ArrayList<String> times= new ArrayList<>();
+
+        times.add("Hourly");
+        times.add("Daily");
+        times.add("Weekly");
+        times.add("Monthly");
+        times.add("Yearly");
+
+        Spinner spinnerCountShoes = findViewById(R.id.time_spinner);
+        ArrayAdapter<String> spinnerCountShoesArrayAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_dropdown_item, times);
+        spinnerCountShoes.setAdapter(spinnerCountShoesArrayAdapter);
     }
+
+
+
+
 
 }
