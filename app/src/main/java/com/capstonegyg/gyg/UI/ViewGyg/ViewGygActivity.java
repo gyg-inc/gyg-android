adedc57fef82caa09a67cb0143515eb7c2c3a637
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-31 13:15:59 -0500

    Added comments to ViewGygActivity.java

--- app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygActivity.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygActivity.java
@@ -27,16 +27,28 @@ public class ViewGygActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //This sets the master view for the ViewGygActivity screen
         setContentView(R.layout.view_gyg_screen);
 
+        //The recycler view that is populated
         viewGygsRecycler = findViewById(R.id.view_gyg_recycler_view);
 
+        //Get the reference to the whole database
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        //Find the specific Firebase node.
         mDatabaseReference = firebaseDatabase.getReference().child("view_gygs");
 
+        /*
+            @arg1 - The "schema" file that defines data.
+            @arg2 - The individual layout that is populated.
+            @arg3 - The class that injects data into the gyg_list_layout
+            @arg4 - The database reference. Holds actual data.
+         */
         mAdapter = new ViewGygFirebaseAdapter(ViewGygData.class, R.layout.gyg_list_layout, ViewGygViewHolder.class, mDatabaseReference);
 
+        //Set the layout manager. (Important) Defines how layout works.
         viewGygsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        //Link the recycler view with the adapter
         viewGygsRecycler.setAdapter(mAdapter);
     }
 }
