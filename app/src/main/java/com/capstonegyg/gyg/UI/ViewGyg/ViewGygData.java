db7eb5c2c49f5aebbc62d88a210f03e6fa8c2425
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-31 13:24:47 -0500

    Added comments to ViewGygData.java and ViewGygFirebaseAdapter.java

--- app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygData.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygData.java
@@ -7,15 +7,23 @@ package com.capstonegyg.gyg.UI.ViewGyg;
  *  Allows User to search and filter by Area and/or user
  */
 
+/**
+   The "schema" file. This is how the JSON node looks like
+*/
+
 public class ViewGygData {
+    //The name of the job
     public String jobName;
+    //The poster's name
     public String jobPosterName;
 
+    //Constructor
     public ViewGygData(String jobName, String jobPosterName) {
         this.jobName = jobName;
         this.jobPosterName = jobPosterName;
     }
 
+    //Default constructor. (Necassary by Firebase standards)
     public ViewGygData() {
     }
 }
--- app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygFirebaseAdapter.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/ViewGyg/ViewGygFirebaseAdapter.java
@@ -7,13 +7,17 @@ import com.google.firebase.database.Query;
 
 public class ViewGygFirebaseAdapter extends FirebaseRecyclerAdapter<ViewGygData, ViewGygViewHolder> {
 
+    //Constructor
     public ViewGygFirebaseAdapter(Class<ViewGygData> modelClass, int modelLayout, Class<ViewGygViewHolder> viewHolderClass, Query query) {
         super(modelClass, modelLayout, viewHolderClass, query);
     }
 
+    //Inject the model data into its respective viewholder/layout widget.
     @Override
     protected void populateViewHolder(ViewGygViewHolder viewHolder, ViewGygData model, int position) {
+        //Set the name of job. Pass in data
         viewHolder.setGygName(model.jobName);
+        //Set the poster name. Pass in data
         viewHolder.setGygPosterName(model.jobPosterName);
     }
 }
