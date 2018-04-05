23fde6505ae3077176894035e4643ad2e230b217
jatimberlake2 <jatimberlake1@crimson.ua.edu>
2018-04-05 00:48:47 -0500

    - Created menu button for ProfileActivity - Created Settings Page for editing user settings - Changed hierarchy settings for ProfileActivity and Settings     in manifest - Added menu folder to res - Some kind of error with MyGygsActivity, so just deleted for     right now

--- app/src/main/java/com/capstonegyg/gyg/UI/Profile/ProfileActivity.java
+++ app/src/main/java/com/capstonegyg/gyg/UI/Profile/ProfileActivity.java
@@ -1,19 +1,29 @@
 package com.capstonegyg.gyg.UI.Profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.capstonegyg.gyg.R;
 import com.capstonegyg.gyg.StartScreen;
 import com.capstonegyg.gyg.UI.ViewGyg.ViewGygActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+/*
+*   Change display name, change skillset, change payment info (send to Shawn), change picture, submit to commit changes
+ */
 
 // add option to delete gyg from user profile
 
@@ -22,15 +32,53 @@ public class ProfileActivity extends AppCompatActivity {
     private ProfileFirebaseAdapter pAdapter;
     private DatabaseReference pDatabaseReference;
     private Button myGygs;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private TextView userName;
 
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
     //DatabaseReference myRef = database.getReference().child("user_profile");
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_settings_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.profile_settings) {
+            Intent i = new Intent(ProfileActivity.this, Settings.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_screen);
         myGygs = findViewById(R.id.profiles_gygs);
+
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+
+            userName = findViewById(R.id.profile_name);
+            userName.setText(name);
+
+
+        }
 
         myGygs.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/capstonegyg/gyg/UI/Profile/Settings.java
@@ -0,0 +1,36 @@
+package com.capstonegyg.gyg.UI.Profile;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.capstonegyg.gyg.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_settings_screen);
+        ActionBar actionBar = this.getSupportActionBar();
+
+        // Set the action bar back button to look like an up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // When the home button is pressed, take the user back to the ProfileActivity
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
