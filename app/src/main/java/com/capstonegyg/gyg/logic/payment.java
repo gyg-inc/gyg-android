da10cd491f6f17af26595e68732751ddc45558b3
smmitchell2 <smmitchell3@gmail.com>
2018-04-23 19:32:18 -0500

    payment.java
    has the needed functions for payment with limits because of free braintree account. Just need to attach the pay button to grab a client token then send payment

---
+++ app/src/main/java/com/capstonegyg/gyg/logic/payment.java
@@ -0,0 +1,64 @@
+package com.capstonegyg.gyg.logic;
+
+import android.content.Context;
+
+import com.braintreepayments.api.BraintreeFragment;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import cz.msebera.android.httpclient.Header;
+
+
+/**
+ * Created by Shawn
+ */
+
+public abstract class payment extends Context {
+
+    private int REQUEST_CODE = 1;
+
+
+/*  This works in settings.java under the profile folder. This contacts the server
+ *  and gets a new clientToken every time */
+//    AsyncHttpClient client = new AsyncHttpClient();
+//        client.get(" https://gyg.herokuapp.com/client_token", new TextHttpResponseHandler() {
+//
+//        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+//
+//        }
+//
+//        public void onSuccess(int statusCode, Header[] headers, String clientToken) {
+//            this.clientToken = clientToken;
+//        }
+//    });
+
+
+    /*Contacts the server to finish payment this will send a payment to the server*/
+//    void postNonceToServer(String nonce) {
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        RequestParams params = new RequestParams();
+//        params.put("payment_method_nonce", nonce);
+//        client.post(" https://gyg.herokuapp.com/checkout", params,
+//                new AsyncHttpResponseHandler() {
+//                    @Override
+//                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+//
+//                    }
+//                }
+//        );
+//    }
+
+    /*A test payment*/
+//    public void onBraintreeSubmit(View v) {
+//        DropInRequest dropInRequest = new DropInRequest()
+//                .clientToken("eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI5YjA3MmM3MWU5MzgxOTVhYjlkNzQ2MjA0ODk4M2QzN2YzNDg3NDNjYTc0OGJjZGM3MWY1ODkzOTQyOGYzMTkyfGNyZWF0ZWRfYXQ9MjAxOC0wNC0yM1QxNzo1NDoxNi43NzAzNzI5ODgrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tLzM0OHBrOWNnZjNiZ3l3MmIifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0=");
+//        startActivityForResult(dropInRequest.getIntent(this), REQUEST_CODE);
+//    }
+
+
+}
